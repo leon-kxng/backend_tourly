4a6fe5866399121a1fe9783097f65d2f827ef63e
leon <leon.kigen@student.moringaschool.com>
2025-05-29 13:27:04 +0300

    feat(migrations): create and update packages collection with fields for title, description, duration, pax, location, price, and image
    - Created a new collection "packages" with fields: id, created, and updated.
    - Updated the "packages" collection to remove created and updated fields, and added new fields: title, description, duration, pax, location, price, and image.
    - Further updated the collection to set list and view rules.
    - Added a reviews field to the collection.
    - Implemented rollback functionality for all changes made to the collection.

---
+++ pb_migrations/1748422303_updated_packages.js
@@ -0,0 +1,171 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3690698942")
+
+  // remove field
+  collection.fields.removeById("autodate2990389176")
+
+  // remove field
+  collection.fields.removeById("autodate3332085495")
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text724990059",
+    "max": 0,
+    "min": 0,
+    "name": "title",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1843675174",
+    "max": 0,
+    "min": 0,
+    "name": "description",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(3, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text2254405824",
+    "max": 0,
+    "min": 0,
+    "name": "duration",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(4, new Field({
+    "hidden": false,
+    "id": "number2307396266",
+    "max": null,
+    "min": null,
+    "name": "pax",
+    "onlyInt": false,
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "number"
+  }))
+
+  // add field
+  collection.fields.addAt(5, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1587448267",
+    "max": 0,
+    "min": 0,
+    "name": "location",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text3402113753",
+    "max": 0,
+    "min": 0,
+    "name": "price",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(7, new Field({
+    "hidden": false,
+    "id": "file3309110367",
+    "maxSelect": 99,
+    "maxSize": 0,
+    "mimeTypes": [],
+    "name": "image",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3690698942")
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "hidden": false,
+    "id": "autodate2990389176",
+    "name": "created",
+    "onCreate": true,
+    "onUpdate": false,
+    "presentable": false,
+    "system": false,
+    "type": "autodate"
+  }))
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "hidden": false,
+    "id": "autodate3332085495",
+    "name": "updated",
+    "onCreate": true,
+    "onUpdate": true,
+    "presentable": false,
+    "system": false,
+    "type": "autodate"
+  }))
+
+  // remove field
+  collection.fields.removeById("text724990059")
+
+  // remove field
+  collection.fields.removeById("text1843675174")
+
+  // remove field
+  collection.fields.removeById("text2254405824")
+
+  // remove field
+  collection.fields.removeById("number2307396266")
+
+  // remove field
+  collection.fields.removeById("text1587448267")
+
+  // remove field
+  collection.fields.removeById("text3402113753")
+
+  // remove field
+  collection.fields.removeById("file3309110367")
+
+  return app.save(collection)
+})
